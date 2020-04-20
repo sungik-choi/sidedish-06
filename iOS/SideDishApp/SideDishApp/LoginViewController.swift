4a012eb94047de4d16526946c4d3ff26136664b1
delma <sdy2856@gmail.com>
2020-04-20 21:11:31 +0900

    [#2] feat :rocket: LoginViewController 생성

---
+++ iOS/SideDishApp/SideDishApp/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  SideDishApp
+//
+//  Created by delma on 2020/04/20.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

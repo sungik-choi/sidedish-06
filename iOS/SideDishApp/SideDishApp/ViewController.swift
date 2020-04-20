61a0f3d7460713589cf7343d2a7b3fd216792e54
delma <sdy2856@gmail.com>
2020-04-20 20:31:22 +0900

    [#2] chore :sparkles: 프로젝트 파일 생성

---
+++ iOS/SideDishApp/SideDishApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideDishApp
+//
+//  Created by delma on 2020/04/20.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

30cb50e213094e808aeb8ac26949a48f63618ab1
Sosah <thstkd92@gmail.com>
2020-04-20 17:58:42 +0900

    [#1] chore :sparkles:​ 프로젝트 빌드
    패키지 설정
    - config, controller, dao, domain(dto, entity), 
    service, utils
    
    MySQL 프로퍼티 설정

---
+++ BE/src/main/java/com/codesquad/sidedish06/SideDish06Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.sidedish06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideDish06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideDish06Application.class, args);
+    }
+
+}
---
+++ BE/src/test/java/com/codesquad/sidedish06/SideDish06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.sidedish06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideDish06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

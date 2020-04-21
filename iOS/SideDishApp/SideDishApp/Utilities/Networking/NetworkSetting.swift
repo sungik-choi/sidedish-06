726ebd004ffbded9e015d4c0f2c4a0ff40b5038e
delma <sdy2856@gmail.com>
2020-04-21 14:57:57 +0900

    [#5] feat :rocket: 네트워크 세팅, 에러케이스 추가

---
+++ iOS/SideDishApp/SideDishApp/Utilities/Networking/NetworkSetting.swift
@@ -0,0 +1,22 @@
+//
+//  APISetting.swift
+//  SideDishApp
+//
+//  Created by delma on 2020/04/21.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+enum EndPoints {
+    static let baseURL = URL(string: "")
+}
+
+

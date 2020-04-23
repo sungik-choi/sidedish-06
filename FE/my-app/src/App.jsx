58ee2e92978d5f68a8787303bd51ff6e4ce51fe5
Sungik Choi <tjddlr92@gmail.com>
2020-04-23 16:35:52 +0900

    [#19] fix :pushpin: : reset 적용 안되는 에러 수정. 재설치

--- FE/my-app/src/App.jsx
+++ FE/my-app/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import GlobalStyle from './components/GlobalStyle';
 import { lnbList, menuList } from './components/MockData';
 
 function App() {
+  useEffect(() => {
+    fetch('https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/main')
+      .then(response => response.json())
+      .then(json => console.log(json));
+  });
+
   return (
     <>
       <GlobalStyle />

e06b93c158165866b60a791bb1f18e829830f9f5
delma <sdy2856@gmail.com>
2020-04-24 12:13:56 +0900

    [#7] feat :rocket: 변경될 모델 필드값으로 받을 Mock, Stub 생성

--- iOS/SideDishApp/SideDishApp/Utilities/Networking/NetworkManager.swift
+++ iOS/SideDishApp/SideDishApp/Utilities/Networking/NetworkManager.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+protocol NetworkManageable {
+    typealias Handler = (Data?, Error?) -> Void
+    
+    func getResource(url: String, methodType: HTTPMethod, body: Data?, completion: @escaping Handler) throws
+}
+
 struct NetworkManager {
     
     func getResource(url: String, methodType: HTTPMethod, body: Data? = nil, completion: @escaping(Result<Data, NetworkErrorCase>) -> Void) {
@@ -24,4 +30,48 @@ struct NetworkManager {
             completion(.success(data))
         }.resume()
     }
+}
+
+struct MockNetworkSuccessSideStub: NetworkManageable {
+    func getResource(url: String, methodType: HTTPMethod, body: Data?, completion: @escaping Self.Handler) throws {
+        let data = """
+        {
+            "menuType": "밑반찬",
+            "menuTypeTitle": "언제 먹어도 든든한 밑반찬",
+            "data": [
+                {
+                    "hash": "HBBCC",
+                    "image": "http://public.codesquad.kr/jk/storeapp/data/fdb0d5fcfb86e332505785225a6d9ade.jpg",
+                    "alt": "[마샐미디쉬] 유자소스 연근무침 250g",
+                    "delivery_type": [
+                        "새벽배송",
+                        "전국택배"
+                    ],
+                    "title": "[마샐미디쉬] 유자소스 연근무침 250g",
+                    "description": "향긋한 유자향과 아삭한 연근",
+                    "originPrice": null,
+                    "salePrice": "6,500원",
+                    "badge": []
+                },
+                {
+                    "hash": "HB9C1",
+                    "image": "http://public.codesquad.kr/jk/storeapp/data/043cf496f07899e7515f761e29d1ffa9.jpg",
+                    "alt": "[너의반찬] 미소된장 고추무침 200g",
+                    "delivery_type": [
+                        "새벽배송",
+                        "전국택배"
+                    ],
+                    "title": "[너의반찬] 미소된장 고추무침 200g",
+                    "description": "고소한 양념 때문에 손이 자꾸 가요",
+                    "originPrice": null,
+                    "salePrice": "4,500원",
+                    "badge": []
+                }
+            ]
+        }
+        """.data(using: .utf8)
+        completion(data, nil)
+    }
+    
+    
 }

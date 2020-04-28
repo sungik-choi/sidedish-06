f9654191a5f946aafb4b507ce44ad57b6e77f773
Sosah <thstkd92@gmail.com>
2020-04-28 18:40:15 +0900

    [#50] chore :sparkles:​ : 프로퍼티수정 및 DB 저장용 컨트롤러 주석처리

--- BE/src/main/java/com/codesquad/sidedish06/controller/SaveController.java
+++ BE/src/main/java/com/codesquad/sidedish06/controller/SaveController.java
@@ -2,36 +2,25 @@ package com.codesquad.sidedish06.controller;
 
 import com.codesquad.sidedish06.service.DetailService;
 import com.codesquad.sidedish06.service.OverviewService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class SaveController {
-
-    private final OverviewService overviewService;
-
-    private final DetailService detailService;
-
-    @GetMapping("/save")
-    public HttpStatus saveOverview() throws IOException, URISyntaxException {
-        overviewService.save();
-        detailService.save();
-        return HttpStatus.OK;
-    }
-
-//    @GetMapping("/detail/mockup")
-//    public Object detail() throws URISyntaxException, JsonProcessingException {
-//        Detail[] details = listDetail();
-//        for (Detail detail : details) {
-//            detailDao.insert(detail);
-//        }
-//        return details;
+        //데이터베이스 저장용컨트롤러 입니다.
+//    private final OverviewService overviewService;
+//
+//    private final DetailService detailService;
+//
+//    @GetMapping("/save")
+//    public String saveOverview() throws IOException, URISyntaxException {
+//        overviewService.save();
+//        detailService.save();
+//        return "OK";
 //    }
 }

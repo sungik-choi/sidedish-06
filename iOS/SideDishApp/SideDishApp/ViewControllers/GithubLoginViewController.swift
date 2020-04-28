f40c8fd6c015e08f26d4a0572a45661ce515024c
delma <sdy2856@gmail.com>
2020-04-28 18:11:54 +0900

    [#3] feat :rocket: Github Login View Controller 생성

---
+++ iOS/SideDishApp/SideDishApp/ViewControllers/GithubLoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GithubLoginViewController.swift
+//  SideDishApp
+//
+//  Created by delma on 2020/04/28.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class GithubLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+
+}

0c94a3b0821d189bc6bfc5c374f82e6f00fe84ab
Sosah <thstkd92@gmail.com>
2020-04-28 19:06:32 +0900

    [#50] chore :sparkles:​ :Github 프로퍼티 암호화

--- BE/src/main/java/com/codesquad/sidedish06/SideDish06Application.java
+++ BE/src/main/java/com/codesquad/sidedish06/SideDish06Application.java
@@ -4,7 +4,6 @@ import com.codesquad.sidedish06.utils.DaoUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 @Slf4j
 public class SideDish06Application {

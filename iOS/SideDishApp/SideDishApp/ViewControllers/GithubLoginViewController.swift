932365dcaeb5c24c4e81655e395f67f87923e91e
delma <sdy2856@gmail.com>
2020-04-28 18:43:13 +0900

    [#3] feat :rocket: github 로그인 화면 띄우기

--- iOS/SideDishApp/SideDishApp/ViewControllers/GithubLoginViewController.swift
+++ iOS/SideDishApp/SideDishApp/ViewControllers/GithubLoginViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
-class GithubLoginViewController: UIViewController {
+class GithubLoginViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
+    @IBOutlet var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView(frame: self.view.frame)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        self.view = self.webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let url = URL(string: "https://github.com/login/oauth/authorize/?client_id=4946b46078dcaa5adfa6&scope=user%20public_repo")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
    
 

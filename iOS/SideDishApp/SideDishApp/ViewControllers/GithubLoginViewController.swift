22e828753359eac69ffbdeb145edf2e039c9cf66
delma <sdy2856@gmail.com>
2020-04-28 22:33:56 +0900

    [#3] feat :rocket: loading indicator 추가

--- iOS/SideDishApp/SideDishApp/ViewControllers/GithubLoginViewController.swift
+++ iOS/SideDishApp/SideDishApp/ViewControllers/GithubLoginViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class GithubLoginViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
     @IBOutlet var webView: WKWebView!
+    @IBOutlet var indicator: UIActivityIndicatorView!
     let successResponse = "http://52.79.117.147/"
     
     override func loadView() {
@@ -38,4 +39,28 @@ class GithubLoginViewController: UIViewController, WKUIDelegate, WKNavigationDel
         decisionHandler(.allow)
     }
     
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        indicator.isHidden = false
+        indicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.isHidden = true
+        indicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        indicator.isHidden = true
+        indicator.stopAnimating()
+        present(networkErrorAlert(), animated: true)
+    }
+    
+    private func networkErrorAlert() -> UIAlertController {
+        let alert = UIAlertController(title: "알림", message: "네트워크에 오류가 생겼습니다. 잠시후 다시 시도해주세요", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default) { _ in
+            self.dismiss(animated: true)
+        }
+        alert.addAction(ok)
+        return alert
+    }
 }

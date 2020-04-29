28657ad3051171a45565a2a3592b7ff20d3e819e
Sungik Choi <tjddlr92@gmail.com>
2020-04-29 14:34:02 +0900

    [#46] chore :sparkles: : 자동 배포를 위한 빌드 파일 업로드
    - gitignore 파일 수정

---
+++ FE/my-app/build/precache-manifest.4548cf9c2b1a67a8d7dd42baa8d8e8f2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516de298426e7a67761e7f9012510b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c82158fa0a3362a5d66",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "2c82158fa0a3362a5d66",
+    "url": "/static/js/2.6052776a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6052776a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e34f1f65bb1595c48cc",
+    "url": "/static/js/main.0dc9019e.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "222fe25d89599e4292bf540b19300ef7",
+    "url": "/static/media/arrow.222fe25d.svg"
+  },
+  {
+    "revision": "a9df70c568dcd7ccf5023bb6d04997ef",
+    "url": "/static/media/logo.a9df70c5.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b1a33f38b18909ae9767c53a834709bd",
+    "url": "/static/media/spinner.b1a33f38.svg"
+  }
+]);

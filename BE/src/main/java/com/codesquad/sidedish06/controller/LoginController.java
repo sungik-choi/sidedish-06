5f15897f2c4e301677df58d6a027b19107eca4ba
Sosah <thstkd92@gmail.com>
2020-04-29 16:37:14 +0900

    [#59] fix :pushpin:​ : bugfix 쿠키 담기

--- BE/src/main/java/com/codesquad/sidedish06/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/sidedish06/controller/LoginController.java
@@ -4,11 +4,14 @@ import com.codesquad.sidedish06.service.LoginService;
 import com.codesquad.sidedish06.utils.UrlUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Log4j2
@@ -19,18 +22,18 @@ public class LoginController {
     private final LoginService loginService;
 
     @GetMapping("/githublogin")
-    public RedirectView githubLogin(@RequestParam("code") String code,
-                                    HttpServletResponse response) {
+    public ResponseEntity<String> githubLogin(@RequestParam("code") String code,
+                                              HttpServletResponse response) {
         loginService.requestAccessToken(code);
-        RedirectView redirectView = new RedirectView();
-        redirectView.setUrl(UrlUtils.MAIN_URL);
         try {
-            response.setHeader("login-check", "true");
-            return redirectView;
-        } catch (RuntimeException e) {
-            response.setHeader("login-check", "false");
-            redirectView.setUrl(UrlUtils.GITHUB_LOGIN_URL);
-            return redirectView;
+            Cookie cookie = new Cookie("login-check", "true");
+            cookie.setPath("/");
+            cookie.setMaxAge(3600);
+            response.addCookie(cookie);
+            response.sendRedirect(UrlUtils.MAIN_URL);
+            return new ResponseEntity<>(HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 }
